652a327a2019378458f53f689bbf5df6f6e03800
rjitsfit <raunak.joshi@sfit.ac.in>
2026-08-14 10:56:37 +0530

    TSC

---
+++ Material/Module_02/2026_08_14/tsc/src/index.ts
@@ -0,0 +1,21 @@
+// TypeScript Datatypes
+
+const randomNumber: number = 54
+let randomName: string = 'asdf'
+let isDone: boolean = true
+
+let x: any = 'Hello'
+x = 123
+
+let ids: number[] = [1, 2, 3]
+ids.push (4)
+
+let emp: [number, string][]
+
+emp = [
+    [1, 'ABC'],
+    [2, 'ASDD']
+]
+
+// Union
+let pid: string | number = 'Hello'
