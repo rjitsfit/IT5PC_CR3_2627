b8ccb1db090d54a10a722bef550ba2b8d7f74911
rjitsfit <raunak.joshi@sfit.ac.in>
2026-08-17 13:26:25 +0530

    Base Commit

---
+++ Experiment_05/fifth-exp/src/App.jsx
@@ -0,0 +1,10 @@
+import Greetings from "./components/Greetings";
+
+export default function App () {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Greetings />    
+    </>
+  )
+}
---
+++ Experiment_05/fifth-exp/src/components/Greetings.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Greetings extends Component {
+    render () {
+        return (
+            <h3>Welcome to the new page</h3>
+        )
+    }
+}
+
+export default Greetings
